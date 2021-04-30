c53eac499881f2151ee9aea520558177c0795f0b
Malachy Lin-Nugent <malachy17@gmail.com>
2021-04-30 09:45:25 -0700

    Made lists for Browse and Discover

--- Health App Redesign/BrowseView.swift
+++ Health App Redesign/BrowseView.swift
@@ -9,7 +9,41 @@ import SwiftUI
 
 struct BrowseView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List {
+                NavigationLink(destination: ContentView()) {
+                    Text("Activity")
+                }
+                NavigationLink(destination: ContentView()) {
+                    Text("Body Measurements")
+                }
+                NavigationLink(destination: ContentView()) {
+                    Text("Cycle Tracking")
+                }
+                NavigationLink(destination: ContentView()) {
+                    Text("Hearing")
+                }
+                NavigationLink(destination: ContentView()) {
+                    Text("Heart")
+                }
+                NavigationLink(destination: ContentView()) {
+                    Text("Mindfulness")
+                }
+                NavigationLink(destination: ContentView()) {
+                    Text("Mobility")
+                }
+                NavigationLink(destination: ContentView()) {
+                    Text("Nutrition")
+                }
+                NavigationLink(destination: ContentView()) {
+                    Text("Respiratory")
+                }
+                NavigationLink(destination: ContentView()) {
+                    Text("Sleep")
+                }
+            }
+            .navigationTitle("Health Categories")
+        }
     }
 }
 
--- Health App Redesign/DiscoverView.swift
+++ Health App Redesign/DiscoverView.swift
@@ -9,7 +9,24 @@ import SwiftUI
 
 struct DiscoverView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        NavigationView {
+            List {
+                NavigationLink(destination: ContentView()) {
+                    Text("Sleep Sound Stories")
+                }
+                NavigationLink(destination: ContentView()) {
+                    Text("Health Records")
+                }
+                NavigationLink(destination: ContentView()) {
+                    Text("Pacer Pedometer & Step Tracker")
+                }
+                NavigationLink(destination: ContentView()) {
+                    Text("Moody Month")
+                }
+            }
+            .navigationTitle("Apps")
+        }
     }
 }
 
