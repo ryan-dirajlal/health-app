88716cb1e95cd8c1e3b24b60ac4c1b8046617527
Malachy Lin-Nugent <malachy17@gmail.com>
2021-04-30 15:44:53 -0700

    Added pics to App recommendations

--- Health App Redesign/BrowseView.swift
+++ Health App Redesign/BrowseView.swift
@@ -112,7 +112,6 @@ struct BrowseView: View {
                 }
             }
         }
-        Spacer()
     }
 }
 
--- Health App Redesign/DiscoverView.swift
+++ Health App Redesign/DiscoverView.swift
@@ -9,44 +9,80 @@ import SwiftUI
 
 struct DiscoverView: View {
     var body: some View {
-        ScrollView {
+        NavigationView {
             VStack {
                 
-                VStack {
-                    Text("Blog Posts")
-                        .font(.title)
+//                Text("Blog Posts")
+//                    .font(.title)
+//
+//                Image("sleep")
+//                    .resizable()
+//                    .frame(width: 300, height: 150, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+//                    .padding(.bottom)
+//
+//                Image("keto")
+//                    .resizable()
+//                    .frame(width: 300, height: 150, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+//
+//
+//                Text("Apps")
+//                    .font(.title)
                 
-                    Image("sleep")
-                        .resizable()
-                        .frame(width: 300, height: 150, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                        .padding(.bottom)
-                    
-                    Image("keto")
-                        .resizable()
-                        .frame(width: 300, height: 150, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                }
-                
-                VStack {
-                    Text("Apps")
-                        .font(.title)
-                    
-                    List {
-                        NavigationLink(destination: ContentView()) {
-                            Text("Sleep Sound Stories")
+                List {
+                    NavigationLink(destination: ContentView()) {
+                        HStack {
+                            Image("sleep-2")
+                                .resizable()
+                                .frame(width: 25, height: 25)
+                            VStack {
+                                Text("Sleep Sound Stories")
+                                Text("Meditation to help you sleep")
+                                    .foregroundColor(.gray)
+                                    .font(.system(size: 8))
+                            }
                         }
-                        NavigationLink(destination: ContentView()) {
-                            Text("Health Records")
+                    }
+                    NavigationLink(destination: ContentView()) {
+                        HStack {
+                            Image("medical-records")
+                                .resizable()
+                                .frame(width: 25, height: 25)
+                            VStack {
+                                Text("Health Records")
+                                Text("Food tracker for weight loss")
+                                    .foregroundColor(.gray)
+                                    .font(.system(size: 8))
+                            }
                         }
-                        NavigationLink(destination: ContentView()) {
-                            Text("Pacer Pedometer & Step Tracker")
+                    }
+                    NavigationLink(destination: ContentView()) {
+                        HStack {
+                            Image("walking")
+                                .resizable()
+                                .frame(width: 25, height: 25)
+                            VStack {
+                                Text("Pace Pedometer")
+                                Text("Walking and weight loss")
+                                    .foregroundColor(.gray)
+                                    .font(.system(size: 8))
+                            }
                         }
-                        NavigationLink(destination: ContentView()) {
-                            Text("Moody Month")
+                    }
+                    NavigationLink(destination: ContentView()) {
+                        HStack {
+                            Image("mood-swings")
+                                .resizable()
+                                .frame(width: 25, height: 25)
+                            VStack {
+                                Text("Moody Month")
+                                Text("Daily wellness and cycle tracker")
+                                    .foregroundColor(.gray)
+                                    .font(.system(size: 8))
+                            }
                         }
                     }
                 }
-                
-            }
+           }
         }
         .navigationTitle("Discover")
         .toolbar{
